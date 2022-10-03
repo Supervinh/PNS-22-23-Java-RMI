283d146aa0c4d81dac165ed455f5e0bfe345e7be
Vinh <narutovinh@hotmail.fr>
2022-10-03 10:28:07 +0200

    Suite

--- Serveur/src/implementations/MovieDesc.java
+++ Serveur/src/implementations/MovieDesc.java
@@ -4,4 +4,5 @@ public class MovieDesc {
     String movieName;
     String isbn;
     String synopsis;
+    Bill bill;
 }
--- Serveur/src/implementations/VODService.java
+++ Serveur/src/implementations/VODService.java
@@ -1,7 +1,10 @@
 package implementations;
 
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class VODService extends UnicastRemoteObject {
 
+    VODService() throws RemoteException {
+    }
 }
