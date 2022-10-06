424edbbdeff5f9d6924f90f9cca9196892e930c6
quentinBourdeau <quentin.bourdeau@etu.univ-cotedazur.fr>
2022-10-06 15:15:46 +0200

    TP2 connection added

--- Serveur/src/implementations/Connection.java
+++ Serveur/src/implementations/Connection.java
@@ -14,19 +14,34 @@ import java.util.List;
 public class Connection extends UnicastRemoteObject implements Remote, IConnection {
 
     List<User> userList;
+    VODService vod;
 
 
     public Connection(int i) throws RemoteException{
         super(i);
+        vod = new VODService(i);
     }
 
     @Override
     public boolean register(String mail, String pwd) throws RemoteException, SignUpFailed {
-        return false;
+        User reg = new User(mail, pwd);
+        if(userList.contains(reg)){
+            throw new SignUpFailed();
+        }
+        else{
+            userList.add(reg);
+        }
+        return true;
     }
 
     @Override
     public IVODService login(String mail, String pwd) throws RemoteException, InvalidCredentialException {
-        return null;
+        User login = new User(mail, pwd);
+        if(userList.contains(login)){
+            return vod;
+        }
+        else{
+            throw new InvalidCredentialException();
+        }
     }
 }
